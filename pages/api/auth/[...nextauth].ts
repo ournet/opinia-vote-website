1f495eda6a94fd02191d3185449a6e342facfb09
Dumitru C <canteadumitru@gmail.com>
2020-12-07 23:14:42 +0200

    initial commit

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,19 @@
+import { NextApiHandler } from "next";
+import NextAuth from "next-auth";
+import Providers from "next-auth/providers";
+import Adapters from "next-auth/adapters";
+import prisma from "../../../lib/prisma";
+
+const authHandler: NextApiHandler = (req, res) => NextAuth(req, res, options);
+export default authHandler;
+
+const options = {
+  providers: [
+    Providers.Facebook({
+      clientId: process.env.FACEBOOK_ID || "",
+      clientSecret: process.env.FACEBOOK_SECRET || ""
+    })
+  ],
+  adapter: Adapters.Prisma.Adapter({ prisma }),
+  secret: process.env.SECRET
+};
---
+++ pages/api/post/index.ts
@@ -0,0 +1,23 @@
+import prisma from "../../../lib/prisma";
+import { getSession } from "next-auth/client";
+import { NextApiRequest, NextApiResponse } from "next";
+
+// POST /api/post
+// Required fields in body: title
+// Optional fields in body: content
+export default async function handle(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const { title, content } = req.body;
+
+  const session = await getSession({ req });
+  const result = await prisma.post.create({
+    data: {
+      title: title,
+      content: content,
+      author: { connect: { email: session?.user?.email || undefined } }
+    }
+  });
+  res.json(result);
+}
